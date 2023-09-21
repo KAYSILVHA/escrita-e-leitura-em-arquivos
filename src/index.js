a12b79a6f03fc2357e6474349bbf8eea3dac9b88
KAYSILVHA <kaysilvha@gmail.com>
2023-09-21 13:27:40 -0300

    ajustes

--- src/index.js
+++ src/index.js
@@ -4,20 +4,31 @@ const app = express()
 app.use(express.json())
 
 
-app.get('/', async (req, res)=>{
+app.get('/', async (req, res) => {
   await fs.writeFile('./src/a.txt', 'Oi')
 })
 
-app.post('/', async (req, res)=>{
-  const {nome, idade} = req.body;
+app.post('/', async (req, res) => {
+  const { nome, idade } = req.body;
+  try {
+    const teste = await fs.readFile('./src/users.json')
+    
+    const pessoas = JSON.parse(teste)
+    
+    pessoas.push({ nome, idade });
+    
+    const pessoasStringFy = JSON.stringify(pessoas)
+    
+    await fs.writeFile('./src/users.json', pessoasStringFy)
+    
+    return res.json('Pessoa cadastrada com sucesso!')
+  } catch (erro) {
+    return res.json(`Deu erro: ${erro.message}`)
+  } finally{
+    console.log("Isso sempre será executado")
+  }
 
-  const teste = await fs.readFile('./src/users.json')
-  const pessoas = JSON.parse(teste)
-  pessoas.push({nome, idade});
-  const pessoasStringFy = JSON.stringify(pessoas)
-  await fs.writeFile('./src/users.json', pessoasStringFy)
-  return res.json('Pessoa cadastrada com sucesso!')
 })
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log("Sua API está rodando na porta 3000")
 })
